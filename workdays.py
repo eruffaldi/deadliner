86297343b52d4ba1f247169b64f4e446a3758f0f
Emanuele Ruffaldi <emanuele.ruffaldi@gmail.com>
2017-05-31 06:56:29 +0200

    improved working days

--- workdays.py
+++ workdays.py
@@ -40,27 +40,27 @@ class BackwardRunner:
 					# before first
 					return False
 
-
+def validatedate(x):
+	if type(x) is str:
+		return datetime.datetime.strptime(x,"%Y/%m/%d").date()
+	elif isinstance(x,datetime.datetime):
+		return x.date()
+	else:
+		return x
+def validaterange(x):
+	if not isinstance(x,tuple):
+		x = validatedate()
+		return (x,x)
+	else:
+		x,y = x
+		x = validatedate(x)
+		y = validatedate(y)
+		if y < x:
+			return (y,x)
+		else:
+			return (x,y)
 def computeworkdays(interval,daysoff,workOnSaturdays=False):
-	def validatedate(x):
-		if type(x) is str:
-			return datetime.datetime.strptime(x,"%Y/%m/%d").date()
-		elif isinstance(x,datetime.datetime):
-			return x.date()
-		else:
-			return x
-	def validaterange(x):
-		if not isinstance(x,tuple):
-			x = validatedate()
-			return (x,x)
-		else:
-			x,y = x
-			x = validatedate(x)
-			y = validatedate(y)
-			if y < x:
-				return (y,x)
-			else:
-				return (x,y)
+
 	first,last = validaterange(interval) 
 
 	# validate days off
@@ -75,20 +75,20 @@ def computeworkdays(interval,daysoff,workOnSaturdays=False):
 	dt = datetime.timedelta(days=-1)
 	br = BackwardRunner(daysoff)
 	wdaySaturday = 5 if not workOnSaturdays else 10
-	print "start from",day,wday
-	print "check saturdays as ",wdaySaturday
+	#print "start from",day,wday
+	#print "check saturdays as ",wdaySaturday
 	while day > first:
 		isworkday = True
 		if wday == 6 or wday == wdaySaturday:
 			#pass
 			isworkday = False
-			print day,wday,"sun/sat"
+			#print day,wday,"sun/sat"
 		elif br.isInside(day):
 			isworkday = False
-			print day,wday,"holiday"
+			#print day,wday,"holiday"
 		if isworkday:
 			r += 1
-		out.append((day,r))
+		out.append(r)
 
 		# back working day and current day
 		if wday == 0:
@@ -97,9 +97,29 @@ def computeworkdays(interval,daysoff,workOnSaturdays=False):
 			wday -= 1
 		day += dt
 
-	print "out"
 
-	return reversed(out)
+	return (first,last,list(reversed(out)))
+
+def workdaysbetween(a,b,f,l,wd):
+	a = validatedate(a)
+	b = validatedate(b)
+	if a > b:
+		b,a=a,b
+	if b < f or a > l:
+		return (b-a).days
+	q = 0
+	if a < f:
+		q = (f-a).days
+		ai = 0
+	else:
+		ai = (a-f).days
+	if b >= l:
+		q += (l-b).days
+		bi = -1
+	else:
+		bi = (b-f).days
+	print "lookup",a,b,"as",ai,bi,"outside days",q
+	return q + (wd[ai]-wd[bi])
 def main():
 	holidays = [
 		("2017/06/04","2017/06/07"),
@@ -107,6 +127,10 @@ def main():
 		("2017/07/01","2017/07/10"),
 		("2017/12/23","2017/12/31")
 	]
-	print "\n".join(["%s\t%d" % q for q in computeworkdays(("2017/06/12",datetime.date.today()),holidays)])
+	f,l,q = computeworkdays(("2017/06/13",datetime.date.today()),holidays)
+	print "\n".join(["%s\t%d" % (f+datetime.timedelta(days=i),q[i]) for i in range(0,len(q))])
+	print workdaysbetween("2017/05/31","2017/06/04",f,l,q)
+	print workdaysbetween("2017/05/31","2017/06/08",f,l,q)
+
 if __name__ == '__main__':
 	main()
